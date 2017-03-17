9667164cd9b87bb29e956eeee4aed191448a9090
lebin <liebin@LebindeMacBook-Pro.local>
2017-03-17 19:20:59 +0800

    Add a simple filter

--- springcloud-zuul-sample/sample-zuul-server1/src/main/java/lebin/sample/zuul/server1/GatewayApplication.java
+++ springcloud-zuul-sample/sample-zuul-server1/src/main/java/lebin/sample/zuul/server1/GatewayApplication.java
@@ -4,6 +4,9 @@ package lebin.sample.zuul.server1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import lebin.sample.zuul.server1.filters.pre.SimpleFilter;
 
 /**
  * GatewayApplication
@@ -18,6 +21,12 @@ public class GatewayApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+	
+	
+	@Bean
+	public SimpleFilter simpleFilter(){
+		return new SimpleFilter();
 	}
 
 }
